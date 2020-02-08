ec6a68c48f6c1080cf16725b99a46c2f12ac08d1
Lucas <abreu.lucass@outlook.com>
2020-02-08 13:37:36 -0300

    challenge 04-4 completed
    finished the fourth challenge from rocketseat's launchbase bootcamp fourth module

--- instructors.js
+++ instructors.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 
 const data = require('./data.json');
 
+const { age, date } = require('./utils')
+
 // show
 exports.show = function(req, res)
 {
@@ -19,9 +21,9 @@ exports.show = function(req, res)
 
     const instructor = {
         ...foundInstructor,
-        age: "",
+        age: age(foundInstructor.birth),
         services: foundInstructor.services.split(','),
-        created_at: "",
+        created_at: new Intl.DateTimeFormat("pt-BR").format(foundInstructor.created_at),
     }
 
     return res.render('instructors/show', { instructor })
@@ -64,5 +66,29 @@ exports.post = function(req, res)
 
         return res.redirect('/instructors')
     })
+}
+
+// edit
+exports.edit = function(req, res)
+{
+    const { id } = req.params
+    
+    const foundInstructor = data.instructors.find(function(instructor)
+    {
+        return instructor.id == id
+    })
+
+    if (!foundInstructor)
+    {
+        res.send('Instructor not found!')
+    }
+
+    const instructor =
+    {
+        ...foundInstructor,
+        birth: date(foundInstructor.birth)
+    }
+
+    return res.render('instructors/edit', { instructor })
 }
 
