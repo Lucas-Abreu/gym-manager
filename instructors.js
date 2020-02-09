5019808f129d030b2d79f386e79454fa19cbaaa7
Lucas <abreu.lucass@outlook.com>
2020-02-09 16:40:06 -0300

    challenge 04-5 completed

--- instructors.js
+++ instructors.js
@@ -92,3 +92,58 @@ exports.edit = function(req, res)
     return res.render('instructors/edit', { instructor })
 }
 
+// put
+exports.put = function(req, res)
+{
+    const { id } = req.body
+    let index = 0
+    
+    const foundInstructor = data.instructors.find(function(instructor, foundIndex)
+    {
+        if (id == instructor.id)
+        {
+            index = foundIndex
+            return true
+        }
+    })
+
+    if (!foundInstructor)
+    {
+        res.send('Instructor not found!')
+    }
+
+    const instructor = 
+    {
+        ...foundInstructor,
+        ...req.body,
+        birth: Date.parse(req.body.birth)
+    }
+
+    data.instructors[index] = instructor
+
+    fs.writeFile('./data.json', JSON.stringify(data, null, 2), function(err)
+    {
+        if (err) return res.send('Write error!')
+
+        return res.redirect(`/instructors/${id}`)
+    })
+}
+
+exports.delete = function(req, res)
+{
+    const { id } = req.body
+    
+    const filteredInstructors = data.instructors.filter(function(instructor)
+    {
+        return instructor.id != id
+    })
+
+    data.instructors = filteredInstructors
+
+    fs.writeFile('./data.json', JSON.stringify(data, null, 2), function(err)
+    {
+        if (err) res.send('Write error!')
+
+        return res.redirect('/instructors')
+    })
+}
